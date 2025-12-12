01463b73d2cb6fc418c0afea2d85e29d8ae27237
bhanuka-viraj <145824067+bhanuka-viraj@users.noreply.github.com>
2025-12-12 15:25:05 +0530

    feat: Implement smooth scrolling, dark mode theming, global styles, and production-ready Next.js configuration.

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,12 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  output: "export",
-  assetPrefix: ".",
+  output: isProd ? "export" : undefined,
+  assetPrefix: isProd ? "." : undefined,
   images: {
     unoptimized: true,
   },
